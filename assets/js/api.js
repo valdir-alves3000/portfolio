ace34769f11d68b02616f72019d23d1514ca91f0
valdir-alves3000 <valdiralves3000@gmail.com>
2023-12-28 10:22:02 -0300

    chore: migrate portfolio to Next.js application

--- assets/js/api.js
+++
@@ -1,7 +0,0 @@
-async function fetchProfileData() {
-  const url =
-    "https://raw.githubusercontent.com/valdir-alves3000/portfolio-design-figma/main/assets/data/profile.json";
-  const response = await fetch(url);
-  const profileData = await response.json();
-  return profileData;
-}
