b3f725f445bc8f6e775573de6878aa3d6d759dfd
valdir-alves3000 <valdiralves3000@gmail.com>
2023-12-29 09:59:23 -0300

    feat(tailwind): add custom background image and animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,10 +9,41 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "404": "url('/assets/img/p404.png')"
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+        "slide-to-left": {
+          from: {
+            transform: "translateX(5%)",
+          },
+          to: {
+            transform: "translateX(-70%)",
+          },
+        },
+        "slide-to-right": {
+          from: {
+            transform: "translateX(-70%)",
+          },
+          to: {
+            transform: "translateX(0%)",
+          },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        "slide-to-left": "100s slide-to-left infinite alternate linear",
+        "slide-to-right": "100s slide-to-right infinite alternate linear",
+      },
+
     },
   },
   plugins: [],
