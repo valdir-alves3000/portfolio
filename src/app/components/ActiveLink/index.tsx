b952144ce8720f78854afdcb6094cb0e31086d24
valdir-alves3000 <valdiralves3000@gmail.com>
2023-12-30 06:34:05 -0300

    feat: add ActiveLink component

---
+++ src/app/components/ActiveLink/index.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { clsx } from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ReactNode } from "react";
+
+interface ActiveLinkProps {
+  children: ReactNode;
+  href: string;
+}
+
+export function ActiveLink({ children, href }: ActiveLinkProps) {
+  const pathname = usePathname();
+
+  return (
+    <Link
+      className={clsx(
+        "m-2 hover:text-violet-400",
+        pathname.includes(href) ? "text-violet-300" : "text-gray-400"
+      )}
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
