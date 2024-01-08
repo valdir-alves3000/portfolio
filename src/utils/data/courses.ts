855403844d4de85803e6ccb19c5ef63aaa31267f
valdir-alves3000 <valdiralves3000@gmail.com>
2024-01-08 09:15:19 -0300

    chore: updating the course list

--- src/utils/data/courses.ts
+++ src/utils/data/courses.ts
@@ -1,34 +1,67 @@
 export const courses = [
   {
+    name: "Trilha Node.JS",
+    certificate: "https://app.rocketseat.com.br/certificates?certificateId=cb4b295f-0f79-4d02-950f-ebb380cd10e7",
+    image: "/assets/img/certificate-nodejs.png"
+  },
+  {
     name: "Formação Scrum Master Certification",
-    certificate: "https://www.dio.me/certificate/B3B65D89/"
+    certificate: "https://www.dio.me/certificate/B3B65D89/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/B3B65D89.jpg"
   },
   {
     name: "Formação TypeScript Fullstack Developer",
-    certificate: "https://www.dio.me/certificate/09357C73/"
+    certificate: "https://www.dio.me/certificate/09357C73/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/09357C73.jpg"
+  },
+  {
+    name: "Formação JavaScript Developer",
+    certificate: "https://www.dio.me/certificate/503187CB/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/503187CB.jpg"
   },
   {
     name: "Linux Experience",
-    certificate: "https://www.dio.me/certificate/7090E953/"
+    certificate: "https://www.dio.me/certificate/7090E953/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/7090E953.jpg"
   },
   {
     name: "Impulso Javascript Evolution",
-    certificate: "https://www.dio.me/certificate/43504E70/"
+    certificate: "https://www.dio.me/certificate/43504E70/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/43504E70.jpg"
+  },
+  {
+    name: "Desenvolvimento de Jogos",
+    certificate: "https://www.dio.me/certificate/2242E302/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/2242E302.jpg"
   },
   {
     name: "Eduzz Full Stack developer #3",
-    certificate: "https://www.dio.me/certificate/7802F964/"
+    certificate: "https://www.dio.me/certificate/7802F964/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/7802F964.jpg"
   },
   {
     name: "GFT  Star #4 .NET",
-    certificate: "https://www.dio.me/certificate/5057CD03/"
+    certificate: "https://www.dio.me/certificate/5057CD03/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/5057CD03.jpg"
   },
   {
     name: "Inter Frontend Developer",
-    certificate: "https://www.dio.me/certificate/3335DCB0/"
+    certificate: "https://www.dio.me/certificate/3335DCB0/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/3335DCB0.jpg"
   },
   {
-    name: "Trilha Nodejs",
-    certificate: "https://drive.google.com/file/d/1NxiI9qec6CYjVbSzfJ__3kiyuJVv0qvL/view"
-  }
+    name: "Formação ChatGPT for Devs",
+    certificate: "https://www.dio.me/certificate/1CBEE74B/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/1CBEE74B.jpg"
+  },
+  {
+    name: "Fortaleza dos CodeMiners",
+    certificate: "https://www.dio.me/certificate/9BD4F239/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/9BD4F239.jpg"
+  },
+  {
+    name: "DIO Campus Expert",
+    certificate: "https://www.dio.me/certificate/92AFF70B/",
+    image: "https://hermes.digitalinnovation.one/certificates/cover/92AFF70B.jpg"
+  },
 ]
